b617689cbae8e20caae7ed3724eb2c6995ab99c9
Ouzar Merouane <ouzar.merouane@gmail.com>
2023-01-14 15:19:42 -0500

    i initialized react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import { useState } from 'react'
-
-
+import axios from 'axios'
+import {useQuery} from '@tanstack/react-query'
 function App() {
   const [count, setCount] = useState(0)
+const {data,isLoading}=useQuery({queryKey:['slt'],
+  queryFn:async()=> {
+    let res = await axios.get('https://api.coinstats.app/public/v1/coins?skip=0&limit=5&currency=EUR')
+  return res.data
+  
+  }
+} 
 
+)
+if(isLoading){
+  return <p>Data is Loading</p>
+}
+console.log(data)
   return (
     <div className="w-screen h-screen bg-blue-800 text-white text-center ">
       <h2 className='text-2xl pt-10'>Hello world</h2>
